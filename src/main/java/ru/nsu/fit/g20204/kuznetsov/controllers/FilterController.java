4f17375d5f4308a45df51f9e52c16b5f76dec667
Rodion Kotov <r.kotov@g.nsu.ru>
2023-03-21 22:34:46 +0700

    Инициализировал классы для сглаживающего фильтра фильтра

--- src/main/java/ru/nsu/fit/g20204/kuznetsov/controllers/FilterController.java
+++ src/main/java/ru/nsu/fit/g20204/kuznetsov/controllers/FilterController.java
@@ -4,20 +4,32 @@ import ru.nsu.fit.g20204.kuznetsov.Hand;
 import ru.nsu.fit.g20204.kuznetsov.controllers.filters.BlackAndWhiteController;
 import ru.nsu.fit.g20204.kuznetsov.controllers.filters.RotationController;
 
+import ru.nsu.fit.g20204.kuznetsov.Hand;
+import ru.nsu.fit.g20204.kuznetsov.History;
+import ru.nsu.fit.g20204.kuznetsov.ui.content.DrawingArea;
+
 import java.awt.*;
+import java.util.logging.Logger;
 
 public class FilterController implements Controller {
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void paint(Graphics g) {
         switch (Hand.getFilterType()) {
             case BLACK_AND_WHITE -> BlackAndWhiteController.performImage();
             case ROTATION -> RotationController.performImage();
+            case SMOOTHING -> {
+                History.getLastScreen();
+                DrawingArea.takeSnapshot();
+            }
         }
     }
 
     public static void beginControl(Point point) {
+        logger.info("filter put");
     }
 
     public static void finishControl(Point point) {
+
     }
 
     public static void mediumControl(Point point) {
---
+++ src/main/java/ru/nsu/fit/g20204/kuznetsov/controllers/filters/SmoothingFilterController.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.g20204.kuznetsov.controllers.filters;
+
+import ru.nsu.fit.g20204.kuznetsov.controllers.Controller;
+
+public class SmoothingFilterController extends BaseFilterController {
+    public static void performImage() {
+
+    }
+}
